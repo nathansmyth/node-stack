7bba4eca08ac996c47a6ea241ee9d199fce190c9
Nathan Smyth <nathan.smyth@sandboxstudio.com>
2014-06-12 17:58:55 -0700

    Add 500 error handling to server app

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
