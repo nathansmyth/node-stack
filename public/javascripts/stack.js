b1d6f7fc75a99a88ad0b3d52a25376b84c250c5d
Nathan Smyth <email@nathansmyth.com>
2014-12-18 20:30:56 -0800

    Update script to collect new item, append to #list, and reset #create box

--- public/javascripts/stack.js
+++ public/javascripts/stack.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-  var stack = stack || {},
-      list_item = '';
-  function stack.append(create_item) {
-    list_item = create_item;
+  var stack = stack || {};
+  stack.latest_item = '';
+
+  $('#create').focus();
+
+  stack.collect = function(create_item) {
+    stack.latest_item = create_item;
   }
+  stack.append = function() {
+    $('#list').append('<li>' + stack.latest_item + '</li>');
+  }
+
   $('#create').keydown(function(e) {
     if(e.keyCode == 13) { // Enter pressed
-      append(this.value);
+      if(this.value == '') { return false; }
+      stack.collect(this.value);
+      stack.append();
+      this.value = '';
     }
   });
 });
