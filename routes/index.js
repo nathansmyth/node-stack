4dc2366abacd4c5cb31218e84659664f10d87f34
Nathan Smyth <email@nathansmyth.com>
2015-05-23 17:19:09 -0700

    Update /lists.json to send actual json data

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
+// GET home page
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'lists' });
 });
 
-router.get('/lists.json', function(req, res) {
-  res.send('lists');
+// GET json representation of lists
+router.get('/lists.json', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ a: 1 }, null, 3));
 });
 
 module.exports = router;
