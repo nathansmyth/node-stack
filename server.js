ea37ef6763aec3d6e4cb83a6aee5279cd3674fb7
Nathan Smyth <email@nathansmyth.com>
2015-07-25 14:49:25 -0700

    Rename internal app variable to api

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var redis = require('ioredis');
 
-var app = express();
+var api = express();
 // var redis = new Redis('/tmp/redis.sock');
 
-app.get('/', function(req, res){
+api.get('/', function(req, res){
   res.send('Hello World');
 });
-app.use(function(err, req, res, next){
+api.use(function(err, req, res, next){
   console.error(err.stack);
   res.send(500, 'Something broke!');
 });
 
-app.set('port', process.env.PORT || 3000);
+api.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
+var server = api.listen(api.get('port'), function() {
   console.log('Listening on port %d', server.address().port);
 });
