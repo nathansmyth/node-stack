16bc4ac36243d04e1dbc3e7675137cb016ed9c5e
Nathan Smyth <email@nathansmyth.com>
2015-08-05 19:52:07 -0700

    Add error handling, endpoints, and view engine w/ views

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var redis = require('ioredis');
+var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/endpoints');
+var endpoints = require('./routes/endpoints');
 var users = require('./routes/users');
 
 var api = express();
 // var redis = new Redis('/tmp/redis.sock');
+
+// view engine setup
+api.set('views', path.join(__dirname, 'views'));
+api.set('view engine', 'jade');
+
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded());
+api.use(cookieParser());
 
 api.get('/', function(req, res){
   res.send('Hello World');
@@ -19,7 +28,44 @@ api.use(function(err, req, res, next){
 });
 
 api.set('port', process.env.PORT || 3000);
+api.set('env', 'development');
 
 var server = api.listen(api.get('port'), function() {
   console.log('Listening on port %d', server.address().port);
+});
+
+api.use(express.static(path.join(__dirname, 'public')));
+
+api.use('/', endpoints);
+api.use('/users', users);
+
+/// error handlers
+
+/// catch 404 and forward to error handler
+api.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (api.get('env') === 'development') {
+  api.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+api.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
