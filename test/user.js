094dce5704a35b8c9069d29c0f181f3125743fb3
Nathan Smyth <email@nathansmyth.com>
2015-09-29 11:52:49 -0700

    Move user test from tests to test folder

---
+++ test/user.js
@@ -0,0 +1,15 @@
+describe('Creating a new User',function(){
+  var user;
+
+  before(function(done){
+    User.create({ username: 'test' }, function(err,u){
+      user = u;
+      done();
+    });
+  });
+
+  it('should have a username',function(){
+    user.should.have.property('username','test');
+  });
+});
+
