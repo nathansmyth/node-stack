5f9d4ea690fbd5ddd8a95d92e3e38eba7e81510a
Nathan Smyth <email@nathansmyth.com>
2015-10-15 01:23:46 -0600

    Remove logger and update error handlers

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var redis = require('ioredis');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -14,7 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
@@ -25,35 +23,30 @@ app.use('/', routes);
 app.use('/users', users);
 
 /// error handlers
-
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+www.use(logErrors);
+www.use(clientErrorHandler);
+www.use(errorHandler);
+function logErrors(err, req, res, next) {
+  console.log('Error Logging', err.stack);
+  next(err);
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    console.log('Error XHR');
+    res.status(500).send({ error: 'Something blew up!' });
+  } else {
+    console.log('Error Other Client');
+    next(err);
+  }
+}
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  console.log('Error 500');
+  res.send({
+    error: err.message
+  });
+}
 
 app.set('port', process.env.PORT || 3000);
 
